797dea14c85a48b5d0f4f279da5dfad02fe51f13
Mariia Stashuk and stephen trewick <stephen@>
2019-04-15 14:43:35 -0700

    back button and sleep table

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,17 @@ $(document).ready(function(){
     event.preventDefault();
 
   });
+
+  $("#sleep-button").click(function(){
+    $("#form").slideUp();
+    $ ("#check-buttons").slideUp();
+    $("#sleep-table").slideDown();
+  });
+
+  $("#back-button").click(function(){
+    $("#form").slideDown();
+    $ ("#check-buttons").slideDown();
+    $("#sleep-table").slideDown();
+    $("#sleep-table").slideUp();
+  });
 });
